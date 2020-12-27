9a2a51455ba4b20b276a1ecf2b56a6be7baa3cd9
Nate Fabian <nathan.fabian@stvincent.edu>
2020-12-27 18:54:46 -0500

    minor changes to server.ts to make look better

--- RootSrc/server.ts
+++ RootSrc/server.ts
@@ -4,13 +4,19 @@ import path from "path";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log(req.path);
+  next();
+});
+
 //import route from "./routes";
 const route = require("./routes");
 app.use(route);
 
 //serve anything in the static folder as is
-app.use(express.static(path.join(__dirname, "../static")));
+//app.use(express.static(path.join(__dirname, "../static")));
 
 app.listen(port, () => {
+  console.log(`http://localhost:3000`);
   console.log(`server running on ${port}`);
 });
